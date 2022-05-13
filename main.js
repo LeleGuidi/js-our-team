a189b3925b2663bffe802ce529847f97ce73cd9e
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-13 15:22:54 +0200

    Creato array di oggetti

---
+++ main.js
@@ -0,0 +1,34 @@
+//Creo un array di oggetti per i dati dei membri
+const members = [
+    {
+        memberName: "Wayne Barnett",
+        memberDuty: "Founder & CEO",
+        memberPicture: "img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        memberName: "Angela Caroll",
+        memberDuty: "Chief Editor",
+        memberPicture: "img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        memberName: "Walter Gordon",
+        memberDuty: "Office Manager",
+        memberPicture: "img/walter-gordon-office-manager"
+    },
+    {
+        memberName: "Angela Lopez",
+        memberDuty: "Social Media Manager",
+        memberPicture: "img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        memberName: "Scott Estrada",
+        memberDuty: "Developer",
+        memberPicture: "img/scott-estrada-developer.jpg"
+    },
+    {
+        memberName: "Barbara Ramos",
+        memberDuty: "Graphic Designer",
+        memberPicture: "img/barbara-ramos-graphic-designer.jpg"
+    }
+]
+console.log(members)
